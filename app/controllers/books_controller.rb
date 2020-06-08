7d1e8868cb67bc4271ed8d2996b6475eba8b0247
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-06-09 04:03:26 +0900

    fix index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,10 +18,9 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-     if book.save
-
-      redirect_to book_path(book), notice: "Book was successfully created."
+    @book = Book.new(book_params)
+     if @book.save
+      redirect_to book_path(@book), notice: "Book was successfully created."
      else
       @books = Book.all
       render 'index'
@@ -33,9 +32,9 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-       redirect_to book_path(book),notice: "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book),notice: "Book was successfully updated."
     else
       render 'edit'
     end
