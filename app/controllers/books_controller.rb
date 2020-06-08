333ade25a2503220cb7fa083bcb5120b8dce02ec
下田直道 <naomichi@shimodanaomichinoMacBook-Air.local>
2020-06-08 22:00:24 +0900

    update index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,12 +6,11 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
- 
 
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -19,9 +18,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-     book.save
-      redirect_to root_path
+    @book = Book.new(book_params)
+     if book.save
+
+      redirect_to book_path(book), notice: "Book was successfully created."
+     else 
+      @books = Book.all
+
+      render 'index'
+     end
   end
 
   def edit
@@ -31,9 +36,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-
-    redirect_to book_path(book)
+    redirect_to book_path(book.id)
   end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+ end
 
   private
 
